389f7382df56facb1c1ae4bf48e2fb58439c19b5
Musbau Adesola <musbauinfo@gmail.com>
2025-09-04 15:38:40 -0500

    jhjfdjsgdwewed

--- main.tf
+++ main.tf
@@ -8,9 +8,8 @@ module "app_bucket" {
   source = "./modules/s3"
   ### tst
   bucket_name          = "tweakideaz-intl-dev-x4y7zq"
-  create_kms_key       = false
-  use_existing_kms_key = true
-
+  create_kms_key       = true
+  use_existing_kms_key = false
 
   kms_key_id = data.aws_kms_key.existing.arn
 
