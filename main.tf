35678fa8377133973f61e2da2e833cff2a974ccc
Musbau Adesola <musbauinfo@gmail.com>
2025-09-04 16:09:18 -0500

    update s3 creation

--- main.tf
+++ main.tf
@@ -1,15 +1,15 @@
-# data "aws_kms_key" "existing" {
-#   key_id = "6a5d782f-bc5b-425e-9fe6-0dd08e065eb3"
-# }
+data "aws_kms_key" "existing" {
+  key_id = "666a98bf-48f6-4af3-b891-c2b87b8950c1"
+}
 
 module "app_bucket" {
   source = "./modules/s3"
   ### tst
   bucket_name          = "tweakideaz-intlweb"
-  create_kms_key       = true
+  create_kms_key       = false
   use_existing_kms_key = false
 
-  # kms_key_id = data.aws_kms_key.existing.arn
+  kms_key_id = data.aws_kms_key.existing.arn
 
   tags = {
     Environment = "prod"
