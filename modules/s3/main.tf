693366373a734ed8a4b6089e9eb094a30896fff2
musbau2013 <musbauinfo@gmail.com>
2025-09-06 17:27:06 -0500

    updated the workflow

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -57,7 +57,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
   rule {
     apply_server_side_encryption_by_default {
       sse_algorithm     = "aws:kms"
-      kms_master_key_id = var.kms_key_id != ""? var.kms_key_id : aws_kms_key.this.arn
+      kms_master_key_id = var.kms_key_id != ""? var.kms_key_id : aws_kms_key.this[0].arn
     }
   
   }
