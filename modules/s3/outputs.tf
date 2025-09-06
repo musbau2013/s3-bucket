f4640a0f6e22cc6d5e528da767a22bc495735ce9
musbau2013 <musbauinfo@gmail.com>
2025-09-06 17:29:09 -0500

    updated the workflow

--- modules/s3/outputs.tf
+++ modules/s3/outputs.tf
@@ -1,12 +1,12 @@
-# output "bucket_arn" {
-#   value = aws_s3_bucket.this.arn
-# }
+output "bucket_arn" {
+  value = aws_s3_bucket.this.arn
+}
 
-# output "bucket_name" {
-#   value = aws_s3_bucket.this.bucket
-# }
+output "bucket_name" {
+  value = aws_s3_bucket.this.bucket
+}
 
-# output "kms_key_arn" {
-#   value       = aws_kms_key.this.arn
-#   description = "KMS key ARN used for bucket encryption"
-# }
+output "kms_key_arn" {
+  value       = aws_kms_key.this[0].arn
+  description = "KMS key ARN used for bucket encryption"
+}
