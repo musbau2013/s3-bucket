2d2e00a24be4c81a62694c96682bf607af66df4b
musbau2013 <musbauinfo@gmail.com>
2025-09-06 18:25:45 -0500

    updated the workflow

--- main.tf
+++ main.tf
@@ -1,15 +1,15 @@
-data "aws_kms_key" "existing" {
-  key_id = "7737ce5f-0274-4d82-8381-67e5649bf3f1"
-}
+# data "aws_kms_key" "existing" {
+#   key_id = "7737ce5f-0274-4d82-8381-67e5649bf3f1"
+# }
 
 module "app_bucket" {
   source = "./modules/s3"
   ### tst
   bucket_name          = "tweakideaz-intlweb"
-  create_kms_key       = false
-  use_existing_kms_key = true
+  create_kms_key       = true
+  use_existing_kms_key = false
 
-  kms_key_id = data.aws_kms_key.existing.arn
+  # kms_key_id = data.aws_kms_key.existing.arn
 
   tags = {
     Environment = "prod"
